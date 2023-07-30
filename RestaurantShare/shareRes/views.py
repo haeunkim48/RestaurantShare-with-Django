f76fa45bb81ef51d5859fc5a19e60ebd87971edf
haeunkim48 <haeunkim48@gmail.com>
2023-07-30 18:14:42 +0900

    iurls&templates setting finish

--- RestaurantShare/shareRes/views.py
+++ RestaurantShare/shareRes/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    # return HttpResponse ("index")
+    return render(request, 'shareRes/index.html')
+
+def restaurantDetail(request):
+    # return HttpResponse ("restaruantDetail")
+    return render(request, 'shareRes/restaruantDetail.html')
+
+def restaurantCreate(request):
+    # return HttpResponse ("restaurantCreate")
+    return render(request, 'shareRes/restaruantCreate.html')
+
+def categoryCreate(request):
+    return render(request, 'shareRes/categoryCreate.html')
